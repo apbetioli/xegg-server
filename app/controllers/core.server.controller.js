142101b1e0daef08216b4296a117e7de80105f1e
apbetioli <apbetioli@gitlab.com>
2014-11-07 16:15:06 -0200

    Adicionado executor de comandos shell para auto atualizar

--- app/controllers/core.server.controller.js
+++ app/controllers/core.server.controller.js
@@ -1,5 +1,7 @@
 'use strict';
 
+var exec = require('exec');
+
 exports.index = function (req, res) {
     res.render('index', {
         user: req.user || null
@@ -9,4 +11,18 @@ exports.index = function (req, res) {
 exports.setCache = function (req, res, next) {
     res.header('Cache-Control', 'public, max-age=315360000');
     next();
+};
+
+exports.gitPull = function(req, res) {
+    exec('git pull origin master',
+        function (error, stdout, stderr) {
+            res.writeHead(200, {'Content-Type': 'text/plain'});
+
+            res.write('stdout: ' + stdout + '\n');
+            res.write('stderr: ' + stderr + '\n');
+            if (error !== null) {
+                res.write('exec error: ' + error);
+            }
+            res.end();
+        });
 };
--- app/routes/core.server.routes.js
+++ app/routes/core.server.routes.js
@@ -4,6 +4,9 @@ module.exports = function(app) {
 
 	var core = require('../../app/controllers/core');
 	app.route('/').get(core.index);
+    app.route('/gitpull')
+        .post(core.gitPull)
+        .get(core.gitPull);
 
 };
 
